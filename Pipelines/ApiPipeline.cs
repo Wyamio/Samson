e900450ad47a387837a20e10f16e9ac7faa84012
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-03-03 23:55:15 +0200

    WIP: Porting...

---
+++ Pipelines/ApiPipeline.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Statiq.CodeAnalysis;
+using Statiq.Common;
+using Statiq.Core;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Generator.Pipelines
+{
+    /// <summary>
+    /// Uses Roslyn to analyze any source files loaded in the previous
+    /// pipeline along with any specified assemblies. This pipeline
+    /// results in documents that represent Roslyn symbols.
+    /// </summary>
+    public class ApiPipeline : Pipeline
+    {
+        public ApiPipeline()
+        {
+            Dependencies.Add(nameof(CodePipeline));
+
+            ProcessModules = new ModuleList(
+                new ExecuteIf(
+                    Config.FromContext(ctx =>
+                        ctx.Outputs.FromPipeline(nameof(CodePipeline)).Any()
+                        || ctx.Settings.GetList<string>(DocsKeys.AssemblyFiles).Count > 0
+                        || ctx.Settings.GetList<string>(DocsKeys.ProjectFiles).Count > 0
+                        || ctx.Settings.GetList<string>(DocsKeys.SolutionFiles).Count > 0),
+                    new ConcatDocuments(nameof(CodePipeline)),
+                    new ExecuteConfig(Config.FromContext(ctx =>
+                        new AnalyzeCSharp()
+                            .WhereNamespaces(ctx.Settings.GetBool(DocsKeys.IncludeGlobalNamespace))
+                            .WherePublic()
+                            .WithCssClasses("code", "cs")
+                            .WithDestinationPrefix(ctx.GetPath(DocsKeys.ApiPath))
+                            .WithAssemblies(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.AssemblyFiles)))
+                            .WithProjects(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.ProjectFiles)))
+                            .WithSolutions(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.SolutionFiles)))
+                            .WithAssemblySymbols()
+                            .WithImplicitInheritDoc(ctx.GetBool(DocsKeys.ImplicitInheritDoc)))),
+                    new ExecuteConfig(Config.FromDocument((doc, ctx) =>
+                    {
+                        // Calculate a type name to link lookup for auto linking
+                        string name = null;
+                        string kind = doc.GetString(CodeAnalysisKeys.Kind);
+                        if (kind == "NamedType")
+                        {
+                            name = doc.GetString(CodeAnalysisKeys.DisplayName);
+                        }
+                        else if (kind == "Property" || kind == "Method")
+                        {
+                            IDocument containingType = doc.GetDocument(CodeAnalysisKeys.ContainingType);
+                            if (containingType != null)
+                            {
+                                name = $"{containingType.GetString(CodeAnalysisKeys.DisplayName)}.{doc.GetString(CodeAnalysisKeys.DisplayName)}";
+                            }
+                        }
+                        if (name != null)
+                        {
+                            ConcurrentDictionary<string, string> typeNamesToLink = ctx.GetService<ConcurrentDictionary<string, string>>();
+                            typeNamesToLink.AddOrUpdate(WebUtility.HtmlEncode(name), ctx.GetLink(doc), (x, y) => string.Empty);
+                        }
+                    }))));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            return await Bootstrapper.Factory
+                .CreateWeb(args)
+                .AddSetting(DocsKeys.SourceFiles, new[]
+                {
+                    "src/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
+                    "../src/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs"
+                })
+                .AddSetting(DocsKeys.ApiPath, new NormalizedPath("api"))
+                .ConfigureServices(services => services
+                    .AddSingleton<ConcurrentDictionary<string, string>>())
+                .RunAsync();
+        }
+    }
+}
