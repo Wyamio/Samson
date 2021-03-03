37b2f37005460d5523d8cac7c596b25956f1fdf2
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-03-03 23:55:15 +0200

    WIP: Porting...

---
+++ Pipelines/CodePipeline.cs
@@ -0,0 +1,20 @@
+﻿using Statiq.Common;
+using Statiq.Core;
+using System.Linq;
+
+namespace Generator.Pipelines
+{
+    /// <summary>
+    /// Loads source files.
+    /// </summary>
+    public class CodePipeline : Pipeline
+    {
+        public CodePipeline()
+        {
+            InputModules = new ModuleList(
+                new ReadFiles(
+                    Config.FromSettings(settings
+                        => settings.GetList<string>(DocsKeys.SourceFiles).AsEnumerable())));
+        }
+    }
+}
---
+++ Pipelines/RenderApiPipeline.cs
@@ -0,0 +1,33 @@
+﻿using Statiq.Common;
+using Statiq.Core;
+using Statiq.Html;
+using Statiq.Razor;
+using System.Linq;
+
+namespace Generator.Pipelines
+{
+    /// <summary>
+    /// Renders and outputs the API pages using the API template layouts
+    /// (this pipeline might take a bit of time).
+    /// </summary>
+    public class RenderApiPipeline : Pipeline
+    {
+        public RenderApiPipeline()
+        {
+            Dependencies.Add(nameof(ApiPipeline));
+
+            ProcessModules = new ModuleList(
+                new ExecuteIf(
+                    Config.FromContext(ctx => ctx.Outputs.FromPipeline(nameof(ApiPipeline)).Any()),
+                    new ConcatDocuments(nameof(ApiPipeline)),
+                    new ProcessShortcodes(),
+                    new RenderRazor()
+                        .WithLayout("/_ApiLayout.cshtml"),
+                    new GatherHeadings(),
+                    new InsertHtml("div#infobar-headings", Config.FromDocument(async (IDocument doc, IExecutionContext ctx) => await ctx.GenerateInfobarHeadingsAsync(doc)))));
+
+            OutputModules = new ModuleList(
+                new WriteFiles());
+        }
+    }
+}
